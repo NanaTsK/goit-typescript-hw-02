07aa109b60fe05a1187fadd2ae5ea470ca7e8efd
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-26 23:59:11 +0200

    generic

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -9,6 +9,8 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 export {};
 
+//* Alternative:
+
 // function merge<T, U>(objA: T, objB: U): T & U {
 // 	return Object.assign({}, objA, objB);
 // }
